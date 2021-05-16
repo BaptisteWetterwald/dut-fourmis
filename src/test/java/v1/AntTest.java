3eb80fe59473d8935748f026473ad1dfbcc650e2
glassabliere <lassabliere.gautier@gmail.com>
2021-05-16 19:16:44 +0200

    Test unitaire v0

---
+++ src/test/java/v1/AntTest.java
@@ -0,0 +1,26 @@
+package v1;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AntTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void deplacerVers() {
+    }
+
+    @Test
+    void deplacementValide() {
+    }
+}
