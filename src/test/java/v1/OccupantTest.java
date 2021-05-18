08e803d0a1e038f481adb6cb8417429338c4305e
glassabliere <lassabliere.gautier@gmail.com>
2021-05-18 11:26:54 +0200

    test occupant

--- src/test/java/v1/OccupantTest.java
+++ src/test/java/v1/OccupantTest.java
@@ -7,43 +7,49 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class OccupantTest {
-
+    Graphe g;
+    Occupant o;
     @BeforeEach
     void setUp() {
+       g= new Graphe(10,10);
+        o = new Occupant(2, 4, g);
     }
 
     @AfterEach
     void tearDown() {
     }
 
-    @Test
-        //crée un occupant et verifie que le x y et grid sont bien ceux que tu as mis
-    void occupant()
-    {
 
-    }
     @Test
     //tu crés un graphe et un occupant et tu regarde si le getgraphe de ton occupant est le meme que le graphe créé
     void getGraphe() {
+        assertEquals(g, o.getGraphe());
     }
 
     @Test
-    //tu crés un occupant et tu regarde si le getX de ton occupant est le meme que le X que tu as mis
+
     void getX() {
+        assertEquals(2, o.getX());
     }
 
     @Test
-    //tu crés un occupant et tu regarde si le getY de ton occupant est le meme que le Y que tu as mis
+
     void getY() {
+        assertEquals(2, o.getX());
     }
 
     @Test
     //tu crés un occupant et tu fais un get X, normalement c'est les memes et après tu fais ton SetX et tu refais un getX voir si la valeur a bien changé et si c'est la bonne
     void setX() {
+
+        o.setX(4);
+        assertEquals(4, o.getX());
     }
 
     @Test
     //tu crés un occupant et tu fais un get X, normalement c'est les memes et après tu fais ton SetX et tu refais un getX voir si la valeur a bien changé et si c'est la bonne
     void setY() {
+        o.setX(4);
+        assertEquals(4, o.getX());
     }
 }
