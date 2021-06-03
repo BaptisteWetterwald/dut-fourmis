36e8a0b185763150a908f8e56b237cd25bf903f2
bwetterwald <Batwett_68700>
2021-06-03 20:57:37 +0200

    Update v2

--- src/test/java/v2/Scenario2.java
+++ src/test/java/v2/Scenario2.java
@@ -3,8 +3,6 @@ package v2;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 public class Scenario2
